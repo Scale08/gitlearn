b06d01be95f975eea77cd933f8900750ba2fb64d
Scale <3048973161@qq.com>
2022-08-23 01:38:38 +0800

    changes
    (cherry picked from commit 6ccc73248bb5b06c10a28b8f2fccd0d3abdd14b3)

--- main.c
+++ main.c
@@ -3,5 +3,7 @@
 int main() {
     printf("Hello, World!\n");
     printf("Here are some changes\n");
+    printf("Some changes from the master branch\n");
+    printf("changes\n");
     return 0;
 }
